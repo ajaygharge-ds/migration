5db3e767cc92f3b62b2ee510d959d8c633360fa1
Ajay Gharge DS <ajay.gharge@digitalsalt.in>
2025-10-07 17:29:03 +0530

    new added

---
+++ wp-content/themes/shelfradar-theme/new-template.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: New
+ */ ?>
+ <?php get_header();
+        $banner = get_field('title');
+        $about= get_field('link');
+        $product= get_field('image');
+    ?>
+
+    <div class="test-container">
+        <h1><?php echo $banner; ?></h1>
+        <a href="<?php echo $about; ?>">About Us</a>
+        <img src="<?php echo $product['url']; ?>" alt="Product Image">
+    </div>
+
+ <?php get_footer(); ?>
